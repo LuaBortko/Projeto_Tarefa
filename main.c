39934e54dea163cbba8d085f60aa59c756593950
LuaBortko <133054603+LuaBortko@users.noreply.github.com>
2023-11-01 00:59:39 +0000

    atualiza

--- main.c
+++ main.c
@@ -4,6 +4,9 @@
 
 int main(){
     char arquivo[]= "tarefa";
+    char arquivo_pri[] = "-";
+    char arquivo_cat[] = "-";
+    char arquivo_cat_pri[] = "-";
     int codigo; //armazena codigo de retorno das funcoes
     lista_tarefas Lt;
     codigo = ler_arquivo(&Lt, arquivo); //chama a funcao de ler arquivo
@@ -12,25 +15,39 @@ int main(){
     }
     int opcao;
     do{
-        print_menu();
-        printf("Digite o numero da opcao desejada: ");
-        scanf("%d", &opcao);
-    if (opcao == 0){
-    }else if(opcao == 1){
-        cria_tarefa(&Lt);
-    }else if(opcao == 2){
-        deleta_tarefa(&Lt);
-    }else if(opcao == 3){
-        listar_tarefas(Lt);
-    }else if(opcao == 4){
-        editar_tarefa(&Lt);
-    }else{
-        printf("Opcao escolhida nao existe\n");
-    }
+      print_menu();
+      printf("Digite o numero da opcao desejada: ");
+      scanf("%d", &opcao);
+      printf("\n");
+      if (opcao == 0){
+      }else if(opcao == 1){
+          cria_tarefa(&Lt);
+      }else if(opcao == 2){
+          deleta_tarefa(&Lt);
+      }else if(opcao == 3){
+          listar_tarefas(Lt);
+      }else if(opcao == 4){
+          editar_tarefa(&Lt);
+      }else if(opcao == 5){
+          filtro_prioridades(Lt);
+      }else if(opcao == 6){
+          filtro_estado(Lt);
+      }else if(opcao == 7){
+          filtro_categoria(Lt);
+      }else if(opcao == 8){
+          filtro_categoria_prioridade(Lt);
+      }else if(opcao == 9){
+          exportar_prioridades(Lt,arquivo_pri);
+      }else if(opcao == 10){
+          exportar_categoria(Lt,arquivo_cat);
+      }else if(opcao == 11){
+          exportar_categoria_prioridade(Lt,arquivo_cat_pri);
+      }else{
+          printf("Opcao escolhida nao existe\n");
+      }
+      codigo = salva_tarefa(Lt, arquivo);
+      if(codigo != 0){
+          printf("Erro ao salvar as tarefas\n");
+      }
     }while(opcao != 0); //loop
-
-    codigo = salva_tarefa(Lt, arquivo);    //chama a funcao ->salva tarefa
-    if(codigo != 0){
-        printf("Erro ao salvar as tarefas\n");
-    }
 }
